427e5265049a8a61a501d7e5a8feac37874d37f2
John Pendenque <pendenquejohn@gmail.com>
2021-04-08 00:14:02 +0200

    Created a Spiders registry

--- management/base.py
+++ management/base.py
@@ -5,7 +5,6 @@ from importlib import import_module
 from typing import Any
 
 from zineb.logger import create_logger
-from zineb.settings import settings
 
 logger = create_logger('Command', to_file=True)
 
@@ -32,50 +31,6 @@ class BaseCommand:
         # add_arguments definition of the Command class
         self.add_arguments(parser)
         return parser
-
-    def _load_project_settings(self):
-        """
-        Load the default project settings (Zineb) updating
-        if needed some values with the settings of the
-        user's project
-
-        Returns
-        -------
-
-            OrderedDict: the base settings file updated with
-            the user's settings and some additional annotations
-        """
-        project = os.environ.get('ZINEB_SPIDER_PROJECT')
-
-        try:
-            project_name, _ = project.split('.', maxsplit=1)
-        except:
-            logger.info(f"Command using global settings since user settings was not specified")
-            # If we can't return a project setting,
-            # just return the general settings
-            return settings
-        else:
-            if project is None:
-                return settings
-
-            module = import_module(project)
-            module_dict = module.__dict__
-            # Update the initial settings with
-            # the settings that the user has
-            # implemented in his project
-            user_settings = OrderedDict()
-            for key, value in module_dict.items():
-                if settings.has_setting(key):
-                    user_settings.update({key: value})
-            new_settings = settings(**user_settings)
-            new_settings.update(
-                {
-                    'project_structure': dict(loaded_spiders=[], middlewares=[]),
-                    'project_name': project_name
-                }
-            )
-
-            return new_settings
 
     def add_arguments(self, parser: ArgumentParser):
         """
--- management/commands/start.py
+++ management/commands/start.py
@@ -1,14 +1,10 @@
-import logging
 import os
-from collections import OrderedDict
 from importlib import import_module
 
-from zineb.app import Zineb
 from zineb.checks.core import checks_registry
 from zineb.logger import create_logger
 from zineb.management.base import BaseCommand
-from zineb.models.datastructure import Model
-from zineb.settings import settings
+from zineb.registry import registry
 
 logger = create_logger('Start', to_file=False)
 
@@ -18,9 +14,21 @@ class Command(BaseCommand):
         parser.add_argument('--name', '-n', required=False, type=str)
 
     def execute(self, namespace=None):
-        new_settings = super()._load_project_settings()
-        project_name = new_settings.get('project_name')
-
+        # HACK: In order to load the correct settings
+        # as per what the user has entered, we
+        # have to reinstantiate the class which
+        # will force an update --; the problem is
+        # the settings file is loaded before the command
+        # (since it is placed in the __init__) and in that
+        # situation, only the global settings are loaded.
+        # By forcing and reinstantiation, the command has
+        # the time to place the project's settings in the
+        # Windows environment and therefore load the settings
+        # file of the project
+        # reloaded_settings = settings(LOADED_SPIDERS=[])
+        project = os.environ.get('ZINEB_SPIDER_PROJECT')
+        project_name, _ = project.split('.', maxsplit=1)
+        
         try:
             # Load the spiders module e.g. project.spiders
             spiders_module = import_module(f'{project_name}.spiders')
@@ -32,49 +40,17 @@ class Command(BaseCommand):
             f"of a project and thus cannot load the 'spiders' module. "
             f"Got {project_name}."))
         else:
-            spiders_module_dict = spiders_module.__dict__
+            registry.populate(spiders_module)
 
-        missing_spiders = []
+        if namespace.name is not None:
+            spider_config = registry.get_spider(namespace.name)
+            spider_config.run()
+        else:
+            registry.run_all_spiders()
 
-        spiders = new_settings.get('SPIDERS')
-        for spider_name in spiders:
-            spider_obj = spiders_module_dict.get(spider_name)
-
-            if spider_obj is not None:
-                new_settings['project_structure']['loaded_spiders'].append((spider_name, spider_obj))                    
-            else:
-                missing_spiders.extend([spider_name])            
-
-        # middlewares = new_settings.get('MIDDLEWARES')
-
-        # Run checks on the new settings that were
+        # TODO: Run checks on the new settings that were
         # provided/updated in order to keep things
         # tight and solid
-        checks_registry._default_settings = new_settings
-        checks_registry.run()
-
-        if missing_spiders:
-            for missing_spider in missing_spiders:
-                logger.info(f"'{missing_spider}' spider was not found in project")
-
-        # Send a signal when mostly everything is started
-        # and that the spiders are ready to receive signals
-        # or start processing urls
-
-        loaded_spiders = new_settings.get('project_structure')['loaded_spiders']
-        if namespace.name is not None:
-            spider = list(filter(lambda x: namespace.name in x, loaded_spiders))
-            if not loaded_spiders:
-                logger.info(f"'{namespace.name}' spider was not found in project")
-            else:
-                try:
-                    _ = spider[0][-1]()
-                except:
-                    raise ValueError(("Spider does not exist. Did you forget to register "
-                    f"'{namespace.name}' in your settings file?"))
-        else:
-            # Finally execute each spider. Normally they don't
-            # return an instance but it would be interesting to
-            # actually register their state
-            for spider in loaded_spiders:
-                _ = spider[1]()    
+        # checks_registry._default_settings = new_settings
+        # checks_registry._default_settings = settings
+        # checks_registry.run()
