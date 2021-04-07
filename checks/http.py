ad4bc5bb9bbc92aca714fab96f09b54ee363fc6a
John Pendenque <pendenquejohn@gmail.com>
2021-04-08 00:34:56 +0200

    Cleaning

--- checks/http.py
+++ checks/http.py
@@ -1,12 +1,3 @@
-# class RequestsMixins:
-#     def _check_url_domain_is_valid(self, url):
-#         parsed_url = urlparse(url)
-#         valid_domains = [
-#             domain for domain in self._default_settings.get('DOMAINS')]
-#         if parsed_url[1] not in valid_domains:
-#             pass
-
-
 from zineb.checks.core import checks_registry
 
 @checks_registry.register(tag='domain')
