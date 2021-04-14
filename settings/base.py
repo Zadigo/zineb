06eb2ba6d09adb6ba1dc2545144c1b7742afc3a0
John Pendenque <pendenquejohn@gmail.com>
2021-04-14 14:58:42 +0200

    Update settings file

--- settings/base.py
+++ settings/base.py
@@ -1,5 +1,5 @@
 """
-Default Zineb settings.
+Default Zineb settings
 """
 
 import os
@@ -8,8 +8,6 @@ import logging
 
 PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
 
-# SETTINGS_FILE = os.path.join(PROJECT_PATH, 'settings', 'base.py')
-
 
 # Register all your spiders from your
 # project here
@@ -17,9 +15,10 @@ PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
 SPIDERS = []
 
 
-# You an limit the scrapping to certain amount of
-# domains. Register the acceptable domains to scrap
-# in the list below
+# Limit scrapping to certain specific amount of
+# domains by requiring that each requests needs
+# to respect the domains in this list before
+# they are sent
 
 DOMAINS = []
 
@@ -27,12 +26,6 @@ DOMAINS = []
 # the scheme is HTTPS
 
 ENSURE_HTTPS = False
-
-
-# Use this configuration file to monitor and configure the
-# way your spider will be crawling the internet
-
-# CONFIGURATION_FILE = os.path.join(PROJECT_PATH, 'settings', 'zineb.conf')
 
 
 # Logging is done primaryly in the console.
@@ -72,14 +65,18 @@ RANDOMIZE_USER_AGENTS = False
 # Use this to set a base set of headers for
 # every HTTP request in the application
 
-DEFAULT_REQUEST_HEADERS = {}
+DEFAULT_REQUEST_HEADERS = {
+    'Accept-Language': 'en',
+    'Accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Referrer': 'https://google.com',
+}
 
 
 # Register all the steps that were run by
 # the application. This includes HTTP requests,
 # download history or file creation history
 
-HISTORY = False
+# HISTORY = False
 
 
 # Set a list of proxies to use within the project.
@@ -99,37 +96,21 @@ RETRY_TIMES = 2
 RETRY_HTTP_CODES = [500, 502, 503, 504, 522, 524, 408, 429]
 
 
-# Database settings
-
-# DATABASE = {
-#     'db': 'zineb.database.connection.SQLite',
-#     'name': 'some_name',
-#     'path': os.path.join(PROJECT_PATH, 'db.sqlite'),
-#     'username': None,
-#     'password': None
-# }
-
-
-# Represents the default headers that will be
-# used for each request sent within your project
-
-DEFAULT_REQUEST_HEADERS = {
-    'Accept-Language': 'en',
-    'Accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Referrer': 'https://google.com',
-}
-
-
 # The main folder to store all your downloaded
 # files from the internet. This folder could
 # also point to a folder in the cloud
 
-AWS_SECRET_KEY = None
+# AWS_SECRET_KEY = None
 
-AWS_SOMETHING_KEY = None
+# AWS_SOMETHING_KEY = None
 
-AWS_STORAGE_URL = 'https://example.com'
+# AWS_STORAGE_URL = 'https://example.com'
 
-MEDIA_FOLDER = os.path.join(PROJECT_PATH, 'media')
+# MEDIA_FOLDER = os.path.join(PROJECT_PATH, 'media')
 
-STORAGE = 'zineb.storages.FileFinder'
+
+# Specify a default storage area for content
+# that is downloaded from the internet or files
+# that were created with the model
+
+# STORAGE = 'zineb.storages.FileFinder'
