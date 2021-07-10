054c7dd42c07f327c7346cecb0b91d155f4265c6
John Pendenque <pendenquejohn@gmail.com>
2021-07-10 16:03:08 +0200

    Code improvements

--- models/expressions.py
+++ models/expressions.py
@@ -1,27 +1,140 @@
-from zineb.utils._html import deep_clean
+from typing import Tuple, Union
+from bs4.element import Tag
+from itertools import chain
+from bs4 import BeautifulSoup
 
 
 class Expression:
-    expressions = []
+    soup = None
 
-    def __str__(self):
-        return f"{self.__class__.__name__}({' '.join(self.expressions)})"
+    def __init__(self, tag: str, attrs: dict={}):
+        self.tag = tag
+        self.attrs = attrs
+        self.elements = []
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}()'
+
+    @staticmethod
+    def parse_condition(expression: str):
+        if '=' not in expression:
+            raise ValueError('Should be an expression')
+        return expression.split('=', maxsplit=1)
+
+    def resolve(self, soup: BeautifulSoup):
+        self.soup = soup
+        self.elements = soup.find_all(self.tag, attrs=self.attrs)
 
 
-class T:
-    def __init__(self, expression, field_name):
-        self.expression = expression
-        self.html_page = None
-        self.field_name = field_name
-        self._cached_results = None
+class IfThen(Expression):
+    def __init__(self, tag: str, if_condition: str, then_condition: str, attrs: dict={}):
+        super().__init__(tag, attrs)
+        self.if_condition = if_condition
+        self.then_condition = then_condition
 
-    def __call__(self):
-        results = self.html_page.find_all(expression)
-        def map_function(result): return deep_clean(result.string)
-        self._cached_results = map(map_function, results)
+    def resolve(self, soup: BeautifulSoup):
+        super().resolve(soup)
+        attr, attr_value = self.parse_condition(self.if_condition)
 
-    def __iter__(self):
-        return next(self._cached_results)
+        def logic(element: Tag):
+            attr_exists = element.has_attr(attr)
+            if attr_exists:
+                if element.attrs[attr] == attr_value:
+                    return True
+            return False
 
-    def __str__(self):
-        return f'<{self.__class.__name__}>'
+        filtered_elements = filter(logic, self.elements)
+
+        def implement_then(element: Tag):
+            element.attrs[attr] = self.then_condition
+            return element
+
+        return map(implement_then, filtered_elements)
+
+
+class And(Expression):
+    def __init__(self, tag: str, *expressions):
+        super().__init__(tag)
+
+        self.expressions = [
+            self.parse_condition(expression) 
+                for expression in expressions
+        ]
+
+    def resolve(self, soup: BeautifulSoup):
+        super().resolve(soup)
+
+        filtered_elements = []
+
+        for element in self.elements:
+            truth_array = []
+            for expression in self.expressions:
+                attr, attr_value = expression
+                attr_exists = element.has_attr(attr)
+                if attr_exists:
+                    if element.attrs[attr] == attr_value:
+                        truth_array.append(True)
+                    else:
+                        truth_array.append(False)
+                else:
+                    truth_array.append(False)
+            result = all(truth_array)
+            if result:
+                filtered_elements.append(element)
+        return filtered_elements
+
+
+class Or(Expression):
+    def __init__(self, tag: str, *expressions):
+        super().__init__(tag)
+
+        self.expressions = [
+            self.parse_condition(expression)
+            for expression in expressions
+        ]
+
+    def resolve(self, soup: BeautifulSoup):
+        super().resolve(soup)
+
+        filtered_elements = []
+
+        for element in self.elements:
+            truth_array = []
+            for expression in self.expressions:
+                attr, attr_value = expression
+                attr_exists = element.has_attr(attr)
+                if attr_exists:
+                    if element.attrs[attr] == attr_value:
+                        truth_array.append(True)
+                    else:
+                        truth_array.append(False)
+                else:
+                    truth_array.append(False)
+            result = any(truth_array)
+            if result:
+                filtered_elements.append(element)
+        return filtered_elements
+
+
+class Q(Expression):
+    def __init__(self, tag: str, attrs: dict={}):
+        pass
+
+
+class Conditions:
+    def __init__(self, soup: BeautifulSoup, *conditions: Tuple[Union[IfThen, Or, And, Q]]):
+        self.conditions = list(conditions)
+        results = [condition.resolve(soup) for condition in conditions]
+        self.chained_results = chain(*results)
+
+    def __repr__(self):
+        return str(list(self.chained_results))
+    
+    def __getitem__(self, index):
+        return list(self.chained_results)[index]
+
+with open('tests/html/test_links.html', encoding='utf-8') as f:
+    soup = BeautifulSoup(f, 'html.parser')
+    # a = Conditions(soup, IfThen('a', if_condition='id=title', then_condition='value2'))
+    c = Conditions(soup, And('a', 'id=title', 'class=title'), Or('a', 'id=other'))
+    print(c)
