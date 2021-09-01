6fdc453401686d8cdc3c1d08763435322a6547f2
Zadigo <pendenquejohn@gmail.com>
2021-09-01 12:00:47 +0200

    Global improvements

--- middlewares/wireframe.py
+++ middlewares/wireframe.py
@@ -1,27 +1,35 @@
 import json
 from collections import OrderedDict
+from io import FileIO
+from zineb.http.request import HTMLResponse
 
 class WireFrame:
     """
     This middleware traces the path the crawler
-    follows in order to get  data on the web page
+    follows in order to get data on the web page
     """
     frame = OrderedDict()
 
-    def __init__(self, response=None, limit='body'):
-        with open('wireframe.json', mode='r') as f:
-            loaded_data = json.load(f)
-            data = loaded_data.copy()
+    def __init__(self, response: HTMLResponse=None, limit: str='body'):
+        if not isinstance(response, HTMLResponse):
+            raise TypeError('Response should be an instance of zineb.response.HTMLResponse')
+        buffer = FileIO('wireframe.json', mode='r+')
+        loaded_data = json.load(buffer)
+        data = loaded_data.copy()
+        self.buffer = buffer
 
         self.wireframe = data
-        self.response = response
+        self._response = response
         self.limit = limit
 
-    def __call__(self, response, limit='body'):
+    def __exit__(self):
+        self.buffer.close()
+
+    def __call__(self, response: HTMLResponse, limit: str='body'):
         self.__init__(response, limit=limit)
         return self.frame
 
-    def add_item(self, tag_name, attrs):
+    def add_item(self, tag_name: str=None, attrs: dict={}):
         tag = self.response.find(tag_name, attrs=attrs)
         parents = list(tag.parents)
         for parent in parents:
