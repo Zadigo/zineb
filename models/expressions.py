9a5a1368b3e1350fc7d8455b00d51c65b06b17c9
Zadigo <pendenquejohn@gmail.com>
2021-09-13 15:59:00 +0200

    Remove duplicate resolution

--- models/expressions.py
+++ models/expressions.py
@@ -19,9 +19,7 @@ class ExpressionMixin:
                 f" retrieve the field object for '{self.field_name}'."))
             )
 
-        field = self.model._get_field_by_name(self.field_name)
-        field.resolve(self._cached_data)
-        return field
+        return self.model._get_field_by_name(self.field_name)
 
     def resolve(self):
         raise NotImplementedError('Expression resolution should be implement by child classes')
@@ -279,52 +277,6 @@ class ExtractDay(DateExtractorMixin, ExpressionMixin):
 #     def resolve(self):
 #         cached_values = self.get_field_cached_values()
 #         return max(cached_values)
-
-
-class DateExtractorMixin:
-    field_name = None
-
-    def __init__(self, value: Any, output_field: Callable=None, date_format: str=None):
-        self.value = value
-        self.date = None
-        self.output_field = output_field
-        self.date_format = date_format
-
-    def resolve(self):
-        from zineb.models.fields import DateField
-
-        output_field = super().get_field_object()
-        if self.output_field is None:
-            self.output_field = output_field
-
-        resolution_field = None
-        if not isinstance(output_field, DateField):
-            resolution_field = DateField()
-
-        resolution_field.date_formats.add(self.date_format)
-        resolution_field.resolve(self.value)
-        
-        # if not isinstance(resolution_field._cached_result, datetime.datetime.date):
-        #     raise TypeError('Date should be a datetime object')
-        self._cached_data = resolution_field._cached_result
-
-
-class ExtractYear(DateExtractorMixin, ExpressionMixin):
-    def resolve(self):
-        super().resolve()
-        return self._cached_data.year
-
-
-class ExtractMonth(DateExtractorMixin, ExpressionMixin):
-    def resolve(self):
-        super().resolve()
-        return self._cached_data.month
-
-
-class ExtractDay(DateExtractorMixin, ExpressionMixin):
-    def resolve(self):
-        super().resolve()
-        return self._cached_data.day
 
 
 # class F(ExpressionMixin):
