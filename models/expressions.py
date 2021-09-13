76825b215d9dfaa51745561b86435f9e319a2ab4
Zadigo <pendenquejohn@gmail.com>
2021-09-13 15:58:51 +0200

    Only accept DateField

--- models/expressions.py
+++ models/expressions.py
@@ -191,6 +191,53 @@ class When:
         return result
 
 
+class DateExtractorMixin:
+    lookup_name = None
+    field_name = None
+
+    def __init__(self, value: Any, output_field: Callable=None, date_format: str=None):
+        self.value = value
+        self.date = None
+        self.output_field = output_field
+        self.date_format = date_format
+
+    def resolve(self):
+        from zineb.models.fields import DateField
+
+        # IMPORTANT: In order to extract a year, a month
+        # a day... from the incoming value, we logically 
+        # should only get to deal with a DateField
+        source_field = super().get_field_object()
+        if not isinstance(source_field, DateField):
+            raise TypeError(LazyFormat('{field} should be an instance of DateField.', field=source_field))
+
+        if self.output_field is None:
+            self.output_field = source_field
+
+        source_field.date_formats.add(self.date_format)
+        source_field.resolve(self.value)
+
+        self._cached_data = source_field._cached_result
+        result = getattr(self._cached_data, self.lookup_name)
+        if isinstance(self.output_field, DateField):
+            return result
+        else:
+            self.output_field._simple_resolve(result, convert=True)
+            return self.output_field._cached_result
+
+
+class ExtractYear(DateExtractorMixin, ExpressionMixin):
+    lookup_name = 'year'
+
+
+class ExtractMonth(DateExtractorMixin, ExpressionMixin):
+    lookup_name = 'month'
+
+
+class ExtractDay(DateExtractorMixin, ExpressionMixin):
+    lookup_name = 'day'
+
+
 # class PositionMixin:
 #     def __init__(self, field: str):
 #         self.field_name = field
