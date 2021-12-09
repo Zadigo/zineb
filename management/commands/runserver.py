480144b98e06613974d6600104986d543e563a50
Zadigo <pendenquejohn@gmail.com>
2021-12-09 12:41:46 +0100

    Commands for running server

--- management/commands/runserver.py
+++ management/commands/runserver.py
@@ -1,13 +1,15 @@
-from utils.reloader import run_simple, run_with_reloader
+import sys
+
 from zineb.management.base import ProjectCommand
-from zineb.utils.reloader import Server
+from zineb.utils.reloader import run_simple, run_with_reloader
+from zineb.utils.servers import get_default_server
 
 class Command(ProjectCommand):
     def add_arguments(self, parser):
         parser.add_argument('--reload', type=bool, default=False)
-
+    
     def execute(self, namespace):
-        server = Server()
+        server = get_default_server()
         if namespace.reload:
             run_with_reloader(server.run, args=(), kwargs={})
         else:
