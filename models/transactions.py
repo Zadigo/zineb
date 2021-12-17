9d4001550b529f29c1197ec9048b742b6fcfda44
Zadigo <pendenquejohn@gmail.com>
2021-12-17 13:37:13 +0100

    Documentation + Improved code

--- models/transactions.py
+++ models/transactions.py
@@ -7,6 +7,8 @@ from zineb.utils.iteration import keep_while
 
 
 class TransactionsRegistry:
+    """Keeps track of all opened transactions"""
+    
     transactions = set()
     
     def __repr__(self):
@@ -18,13 +20,16 @@ class TransactionsRegistry:
     def new(self, model, transaction_instance):
         self.transactions.add((model, transaction_instance))
         
+    def close(self, model):
+        del self.transactions[model]
+        
 transactions_registry = TransactionsRegistry()
 
 
 class Transaction:
-    """An interface for a model that can create specific
-    states for a given model therefore allowing rollbacks
-    either to a specific point or to the initial state
+    """An interface which creates specific states of a model's
+    data therefore allowing rollbacks either to a specific point 
+    or to a initial one
     """
     def __init__(self, model=None):
         self.savepoints = defaultdict(list)        
@@ -58,15 +63,21 @@ class Transaction:
     
     def savepoint(self):
         savepoint_name = create_random_string(lowercased=True)
-        model_data = self.model._cached_result
+        try:
+            model_data = self.model._cached_result
+        except:
+            # If the element does not have a _cached_result,
+            # then it's not a model instance
+            raise TypeError(LazyFormat("'{obj}' is not an instance of Model", obj=self.model))
         self.savepoints.update({savepoint_name: model_data.copy_values()})
         return savepoint_name
     
     def rollback(self, savepoint: str=None):
         if savepoint is None:
-            self.model.values = self.savepoints[self._initial_savepoint]
+            data_to_use = self.savepoints[self._initial_savepoint]
         else:
-            self.model.values = self.savepoints[savepoint]
+            data_to_use = self.savepoints[savepoint]
+        self.model._cached_result._rollback_data_to(data_to_use)
     
     def rollback_on_error(self):
         """Rollback container on the first initial
@@ -78,6 +89,8 @@ class Transaction:
 
 
 def transaction(model: Callable):
+    """Opens a new transaction by binding a model
+    to a Transaction class"""
     existing_transaction = list(keep_while(lambda m: model in m, transactions_registry))
     if existing_transaction:
         _, instance = existing_transaction
@@ -86,3 +99,24 @@ def transaction(model: Callable):
         transactions_registry.new(model, instance)
         
     return instance
+
+
+def atomic(model):
+    """A decorator that allows you to wrap a definition
+    as being part of a transaction
+    
+    Example
+    -------
+    
+        class TestSpider(Model):
+            @atomic(SimpleModel)
+            def start(self, response, request, transaction, **kwargs):
+                transaction.model.add_value('name', 'Kendall')
+                transaction.savepoint()
+    """
+    def wrapper(func):        
+        def start(self, response=None, request=None, transaction=None, **kwargs):
+            transaction = Transaction(model=model())
+            return func(self, response=response, request=request, transaction=transaction, **kwargs)
+        return start
+    return wrapper
