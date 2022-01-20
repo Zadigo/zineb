bcb4fb71f0127d989293fbdf52ef2147cfaf8ccb
Zadigo <pendenquejohn@gmail.com>
2022-01-21 00:03:07 +0100

    Create new generator + Fix on name/attrs filter

--- html_parser/utils.py
+++ html_parser/utils.py
@@ -28,28 +28,33 @@ def filter_by_name(data, name: str):
             yield item
 
 
-def filter_by_attrs(data, attrs: dict={}):
+def filter_by_attrs(data, attrs: dict):
     """Filter tags by attributes"""
-    for attr, value in attrs.items():
+    if attrs:    
+        for attr, value in attrs.items():
+            for item in data:
+                result = item.get_attr(attr)
+                if result == value:
+                    yield item
+    else:
         for item in data:
-            result = item.get_attr(attr)
-            if result == value:
-                yield item
-
+            yield item
 
 def filter_by_name_or_attrs(data, name, attrs={}):
     """Filter by name or attributes"""
     tags_by_name = filter(lambda x: x.name == name, data)
 
-    def attrs_filtering_option(x):
-        if attrs:
-            for attr, value in attrs.items():
-                result = x.get_attr(attr)
-                if result == value:
-                    return x
-        else:
-            return x
-    return (attrs_filtering_option(x) for x in tags_by_name)
+    # def attrs_filtering_option(x):
+    #     if attrs:
+    #         for attr, value in attrs.items():
+    #             result = x.get_attr(attr)
+    #             if result == value:
+    #                 return x
+    #     else:
+    #         return x
+    # return (attrs_filtering_option(x) for x in tags_by_name)
+    tags_by_name = filter_by_name(data, name)
+    return filter_by_attrs(tags_by_name, attrs)
 
 
 def break_when(func, items):
