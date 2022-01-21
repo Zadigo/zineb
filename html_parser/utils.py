f66863c6acf4c078ccee94bd967d9e3d08171020
Zadigo <pendenquejohn@gmail.com>
2022-01-21 15:22:21 +0100

    Documentation + Bug fix

--- html_parser/utils.py
+++ html_parser/utils.py
@@ -1,4 +1,4 @@
-from ctypes import Union
+from typing import Callable, Iterator, Union, Generator
 
 HTML_TAGS = {
     'html', 'body', 'main', 'p', 'a', 'br', 'table', 'td', 'tr',
@@ -17,49 +17,51 @@ HTML_TAGS = {
 
 SELF_CLOSING_TAGS = {
     'link', 'br', 'base', 'embed', 'input', 'hr',
-    'meta', 'video', 'img'
+    'meta', 'video', 'img', 'area', 'br', 'col',
+    'param', 'source', 'track', 'wbr'
 }
 
 
-def filter_by_name(data, name: str):
-    """Filter tags by name"""
-    for item in data:
+def filter_by_name(items, name):
+    """Filter by name"""
+    for item in items:
         if item.name == name:
             yield item
 
 
-def filter_by_attrs(data, attrs: dict):
-    """Filter tags by attributes"""
-    if attrs:    
+def filter_by_attrs(items, attrs):
+    """Filter by attributes"""
+    # Even though attrs is required,
+    # it can come as an empty dict
+    # especially for tags that do
+    # not have attributes. This
+    # causes the iteration function
+    # to be skipped and return []
+    # giving the impression that
+    # no tags exists especially when 
+    # used in combination with filter_by_name
+    if attrs:
         for attr, value in attrs.items():
-            for item in data:
+            for item in items:
                 result = item.get_attr(attr)
                 if result == value:
                     yield item
     else:
-        for item in data:
+        for item in items:
             yield item
 
-def filter_by_name_or_attrs(data, name, attrs={}):
-    """Filter by name or attributes"""
-    tags_by_name = filter(lambda x: x.name == name, data)
-
-    # def attrs_filtering_option(x):
-    #     if attrs:
-    #         for attr, value in attrs.items():
-    #             result = x.get_attr(attr)
-    #             if result == value:
-    #                 return x
-    #     else:
-    #         return x
-    # return (attrs_filtering_option(x) for x in tags_by_name)
+def filter_by_name_or_attrs(data, name, attrs):
+    """Filter by name and attributes"""
     tags_by_name = filter_by_name(data, name)
     return filter_by_attrs(tags_by_name, attrs)
 
 
-def break_when(func, items):
-    """Iterate over a list of items and
-    breaks on the first match"""
+def break_when(func: Callable, items: Union[Iterator, Generator]):
+    """
+    A function that immediately breaks
+    when an element is matched for 
+    optimization
+    """
     for item in items:
         if func(item):
             break
