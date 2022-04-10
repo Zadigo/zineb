0824c88b9eac34d15dd939384a10ce4b75f003f1
Zadigo <pendenquejohn@gmail.com>
2022-04-10 12:53:32 +0200

    Documentation

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 def setup():
-    # from zineb.settings import settings
+    """Initial entrypoint that allows the configuration
+    of for a Zineb project by populating the application
+    with the spiders, models and other required items
+    for a project to run"""
     from zineb.registry import registry
-
+    
     registry.populate()
