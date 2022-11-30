fd6cfc2c23e4e90e4cbeaa1db661c4e518b8225c
Zadigo <pendenquejohn@gmail.com>
2022-11-30 20:00:04 +0100

    Update server

--- server/static/router.js
+++ server/static/router.js
@@ -1,52 +1,27 @@
-const routes = [
-    {
-        path: '/',
-        component: BaseLayout,
-        children: [
-            {
-                name: 'home',
-                path: '',
-                components: {
-                    default: SpidersView
-                }
-            },
-            {
-                name: 'spider',
-                path: 'spider/:id(\d+)',
-                components: {
-                    default: SpiderView
-                }
-            },
-            {
-                name: 'settings',
-                path: 'settings',
-                components: {
-                    default: SettingsView
-                }
-            }
-        ]
-
-    }
-]
-
-var router = VueRouter.createRouter({
+const router = VueRouter.createRouter({
     history: VueRouter.createWebHistory(),
-    routes
+    routes: [
+        {
+            path: '/',
+            component: BaseLayout,
+            children: [
+                {
+                    path: '',
+                    name: 'home_view',
+                    component: SpidersView
+                },
+                {
+                    path: 'spider/:id(\d+)',
+                    name: 'spider_view',
+                    component: SpiderView
+                },
+                {
+                    path: 'settings',
+                    name: 'settings_view',
+                    component: SettingsView
+                }
+            ]
+
+        }
+    ]
 })
-
-
-//     {
-            //         name: 'create',
-            //         path: 'create',
-            //         components: {
-            //             left: createPage,
-            //             right: null
-            //         }
-            //     }
-            // ]
-            // components: {
-            //     default: indexPage
-            // },
-            // beforeRouteEnter (to, from, next) {
-
-            // }
--- server/static/views/SpiderView.js
+++ server/static/views/SpiderView.js
@@ -2,23 +2,33 @@ var SpiderView = {
     name: 'SpiderView', 
     template: `
     <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Spider name</h5>
-            
-            <hr>
+        <div class="card-header">
+            <h5 class="card-title">Spider name</h5>    
+        </div>
 
-            <h5 class="mb-3">Urls</h5>
+        <div class="card-body">
+            <h5 class="card-title">Urls</h5>
             
-            <ul class="list-group">
-                <a href="http://example.com" class="list-group-item d-flex justify-content-between">
+            <div class="list-group">
+                <a href="http://example.com" class="list-group-item d-flex justify-content-between align-items-center" target="_blank">
                     <span>http://example.com</span>
                     <i class="fa-solid fa-arrow-up-right-from-square"></i>
                 </a>
-            </ul>
+            </div>
+
+            <div class="my-2">
+                <h5 class="card-title">Proxies</h5>
+
+                <div v-for="(proxy, i) in proxies" :key="i" class="d-flex justify-content-start gap-1 my-1">
+                    <input v-model="proxies[i].netloc" type="text" class="form-control" placeholder="Netloc">
+                    <input v-model="proxies[i].address" type="text" class="form-control" placeholder="Proxy address">
+                    <button type="button" class="btn btn-info" @click="addProxy">Add</button>
+                </div>
+            </div>        
         </div>
 
-        <div class="card-footer text-right">
-            <button class="btn btn-primary">
+        <div class="card-footer d-flex justify-content-end">
+            <button class="btn btn-primary" @click="runSpider">
                 <div v-if="running" class="spinner-grow spinner-grow-sm text-light  me-2" role="status">
                     <span class="visually-hidden">Loading...</span>
                 </div>
@@ -29,10 +39,33 @@ var SpiderView = {
         </div>
     </div>
     `,
-    data: () => ({
-        running: true
-    }),
-    beforeMount () {
-        this.setTitle('Spider')
+    data () {
+        return {
+            running: false,
+            proxies: [
+                {
+                    netloc: null,
+                    address: null 
+                }
+            ]
+        }
+    },
+    methods: {
+        async runSpider () {
+            this.running = true
+        },
+        addProxy () {
+            this.proxies.push({
+                netloc: null,
+                address: null
+            })
+        }
+    },
+    created () {
+        try {
+            this.$store.dispatch('setCurrentSpider', this.$route.params.id)
+        } catch (e) {
+            console.error('some', e)
+        }
     }
 }
