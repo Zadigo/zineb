628d04c2049c6c791b853c95cac79d5d9bd024f7
Zadigo <pendenquejohn@gmail.com>
2023-01-21 16:43:27 +0100

    Refactoring of global test runner

--- tests/runtests.py
+++ tests/runtests.py
@@ -5,7 +5,7 @@ import unittest
 if __name__ == '__main__':
     loader = unittest.TestLoader()
     start_dir = pathlib.Path('.').joinpath('tests')
-    suite = loader.discover(start_dir)
+    suite = loader.discover(f'zineb.{str(start_dir)}')
 
     runner = unittest.TextTestRunner()
     runner.run(suite)
